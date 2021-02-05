2bf9b52a0d788768cd57181de33f43850ef21e77
noelanimanini <noelani26@crystals-MacBook-Pro.local>
2021-02-05 14:44:23 -0800

    I guess I did a-MOUNT to the task

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 // create your App component here
+import React from 'react'; 
+
+export default class App extends React.Component {
+    componentDidMount () {
+        fetch('http://api.open-notify.org/astros.json').then(response => response.json()).then(object=>console.log(object))
+    }
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
